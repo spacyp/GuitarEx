90b27571aa7dd95445d64e3e4de346f37bac0e0f
= <=>
2023-05-07 19:54:25 +0200

    first prototype implementation of an exercise (four fret chromatic run)

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
 import guitarpro
-
-
-def parse_gp_sample():
-    song = guitarpro.parse("input/sample.gp5")
-    song
+import random
 
 
 def create_gp_sample():
     song = guitarpro.Song()
     track = song.tracks[0]
-    track.name = 'My first track'
+    track.name = 'Exercise Track'
     track.channel.instrument = 29  # overdrive guitar
-    voice = song.tracks[0].measures[0].voices[0]
     track.useRSE = True
-    for i in range(16):
-        beat = guitarpro.Beat(voice)
-        beat.duration = guitarpro.Duration(value=guitarpro.Duration.sixteenth)
-        note = guitarpro.Note(beat)
-        note.value = i % 4 + 1
-        note.string = 6 - i // 4
-        beat.notes.append(note)
-        voice.beats.append(beat)
+
+    note_generator = generate_notes()
+
+    #remove default measure
+    song.measureHeaders = []
+    track.measures = []
+
+    finished = False
+    while not finished:
+        song.newMeasure()
+        measure = track.measures[-1]
+        voice = measure.voices[0]
+        measure_is_full = False
+        fullness = 0.0
+        while not measure_is_full:
+            try:
+                note_data = next(note_generator)
+            except StopIteration:
+                finished = True
+                break
+            beat = guitarpro.Beat(voice, duration=guitarpro.Duration(value=note_data.get("duration")))
+            note = guitarpro.Note(beat, value=note_data.get("fret"), string=note_data.get("string"))
+            beat.notes.append(note)
+            voice.beats.append(beat)
+            fullness += 1/note_data.get("duration")
+            if fullness >= 0.99:
+                measure_is_full = True
+
     guitarpro.write(song, "output/test.gp5")
+
+
+def generate_randomized_shape():
+    return random.sample([1, 2, 3, 4], 4)
+
+
+def generate_notes():
+    fret_shape = generate_randomized_shape()
+    print(f"using {fret_shape}")
+    for string in range(6, 0, -1):
+        for fret in fret_shape:
+            yield {"string": string, "fret": fret, "duration": 16}
+    for string in range(1, 7):
+        for fret in reversed(fret_shape):
+            yield {"string": string, "fret": fret, "duration": 16}
+
 
 if __name__ == '__main__':
     create_gp_sample()
-    parse_gp_sample()
-
